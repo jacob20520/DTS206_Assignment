c3d58013cbe6765d6ea48ad5728ca547bfc93d04
Jacob McLaughlin <jacob20520@gmail.com>
2026-08-23 23:27:47 +0100

    Created VPC resource and subnets

---
+++ infrastructure/network.tf
@@ -0,0 +1,21 @@
+resource "aws_vpc" "medicore" {
+  cidr_block           = var.vpc_cidr
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "${var.project_name}-vpc"
+  }
+}
+
+resource "aws_subnet" "public_bastion" {
+  vpc_id                  = aws_vpc.medicore.id
+  cidr_block              = var.public_subnet_cidr
+  availability_zone       = data.aws_availability_zones.available.names[0]
+  map_public_ip_on_launch = false
+
+  tags = {
+    Name = "${var.project_name}-subnet-public-bastion"
+    Tier = "Public"
+  }
+}
--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -15,3 +15,27 @@ variable "environment" {
   type        = string
   default     = "assignment"
 }
+
+variable "vpc_cidr" {
+  description = "CIDR range for the MediCore VPC."
+  type        = string
+  default     = "10.20.0.0/16"
+}
+
+variable "public_subnet_cidr" {
+  description = "CIDR range for the public bastion subnet."
+  type        = string
+  default     = "10.20.10.0/24"
+}
+
+variable "private_subnet_cidr" {
+  description = "CIDR range for the private web/application subnet."
+  type        = string
+  default     = "10.20.20.0/24"
+}
+
+variable "restricted_subnet_cidr" {
+  description = "CIDR range for the restricted database subnet."
+  type        = string
+  default     = "10.20.30.0/24"
+}
